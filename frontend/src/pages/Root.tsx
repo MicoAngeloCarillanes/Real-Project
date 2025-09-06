60ae96bf6e56756117ae78840827dab1087d91e3
Julius <juliustolentino.diamond@gmail.com>
2025-09-06 20:56:56 +0800

    refactor code

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -1,6 +1,15 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 export default function Root() {
+    const location = useLocation();
+    const navigate = useNavigate();
+    
+    useEffect(() => {
+        if (location.pathname === '/') {
+            navigate('/student');
+        }
+    }, [location.pathname]);
 
     return (
         <>
